4408194187a511fd2f6524baa301b0ea1d74b095
Md-Nadim <mdnadimhossain295458@gmail.com>
2020-08-10 01:35:35 -0700

    practise

---
+++ destructure.js
@@ -0,0 +1,7 @@
+const person = { name: 'Jack William',age: 17, job: 'facebooker', gfName: 'Ema Watson', address: 'Kochu khet', phone: '01714253678', friend: ['Tom hancks', 'tom cruise', 'Tom yarn']};
+const gfName = person.gfName;
+const phone = person.phone;
+console.log(gfName, phone);
+console.log(gfName, phone);
+console.log(gfName, phone);
+console.log(gfName, phone);
